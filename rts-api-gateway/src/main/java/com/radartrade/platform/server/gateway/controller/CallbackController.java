bf73959c27cb6cc36424d0342aa3a291d1751242
Khoi Pham <dinhkhoi26031997@gmail.com>
2025-08-23 00:57:11 +0700

    change redirect uri

--- rts-api-gateway/src/main/java/com/radartrade/platform/server/gateway/controller/CallbackController.java
+++ rts-api-gateway/src/main/java/com/radartrade/platform/server/gateway/controller/CallbackController.java
@@ -1,12 +1,19 @@
+// java/com/radartrade/platform/server/gateway/controller/CallbackController.java
+
 package com.radartrade.platform.server.gateway.controller;
 
 import com.radartrade.platform.server.gateway.dto.response.ResponseToken;
 import com.radartrade.platform.server.gateway.service.ExchangeTokenService;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class CallbackController {
@@ -17,17 +24,20 @@ public class CallbackController {
         this.exchangeTokenService = exchangeTokenService;
     }
 
-    /**
-     * @fixme: check state == state stored in session follow oauth2 flow aspect of security
-     * @param code
-
-     * @return
-     */
     @GetMapping("/callback")
-    public ResponseEntity<ResponseToken> callback(@RequestParam String code) {
-        return ResponseEntity.ok(
-                exchangeTokenService.exchangeToken(code)
-        );
+    public ResponseEntity<Void> callback(@RequestParam String code, @RequestParam(required = false) String state) {
+        ResponseToken tokens = exchangeTokenService.exchangeToken(code);
+        URI redirectUri = UriComponentsBuilder.fromUriString("http://localhost:5173/auth/receive-token")
+                .queryParam("token", tokens.token())
+                .queryParam("refreshToken", tokens.refreshToken())
+                .queryParam("idToken", tokens.idToken())
+                .build()
+                .toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(redirectUri);
+
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
-
